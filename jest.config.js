624ca1af90d537e16600f07192e445a2a4f3d268
Noe Sanchez <noeszc@gmail.com>
2021-09-16 15:28:40 -0500

    cleanup tests

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,11 @@
-console.log('called')
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
-  moduleNameMapper: {
-    // Handle CSS imports (with CSS modules)
-    // https://jestjs.io/docs/webpack#mocking-css-modules
-    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
-    // Handle CSS imports (without CSS modules)
-    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/style-mock.js',
-
-    // Handle image imports
-    // https://jestjs.io/docs/webpack#handling-static-assets
-    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': `<rootDir>/__mocks__/file-mock.js`,
-
-    // Handle module aliases
-    '^@/components/(.*)$': '<rootDir>/components/$1',
-  },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  modulePaths: ['node_modules', '<rootDir>/src'],
+  setupFilesAfterEnv: ['<rootDir>/testing/setup-global-fetch.js'],
+  globalSetup: '<rootDir>/testing/setup-env.js',
   transform: {
-    // Use babel-jest to transpile tests with the next/babel preset
-    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
-    '^.+\\.(js|jx|ts|tsx)$': 'ts-jest',
+    '^.+\\.(js|ts)$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
 }
